72efc9a84e20e81154d1b7eecceb5ceecd177ba7
ianasriaz <ianasriaz@example.com>
2025-12-30 10:59:54 +0500

    Fix DropdownButtonFormField parameter: initialValue -> value

--- lib/screens/manage_services_screen.dart
+++ lib/screens/manage_services_screen.dart
@@ -101,7 +101,7 @@ class _ManageServicesScreenState extends State<ManageServicesScreen> {
                         Expanded(
                           flex: 2,
                           child: DropdownButtonFormField<String>(
-                            initialValue: selectedUnit,
+                            value: selectedUnit,
                             icon: const Icon(Icons.keyboard_arrow_down_rounded),
                             decoration: InputDecoration(
                               labelText: 'Unit',
